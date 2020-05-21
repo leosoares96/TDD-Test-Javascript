00132bb159b0a73243b59bcad746a173f7bcfd2f
Leonardo Soares <leo.soares64@outlook.com>
2020-05-21 19:22:18 -0300

    test da função de procurar usuario

--- myProgram.js
+++ myProgram.js
@@ -1,5 +1,25 @@
 const somar = (n1, n2) => n1 + n2
 
+const procurarUsuario = (nome) => {
+
+    jsonUser = [
+        {nome: 'Leonardo'},
+        {nome: 'Pamela'},
+        {nome: 'Nick'}
+    ];
+
+    if(nome){
+        let user = jsonUser.filter((user) => {
+            user.nome === nome;
+        })
+
+        return (user == '')?'Usuário não encontrado':nome;
+        
+    }else{
+        return 'Campo nome esta vazio'
+    }
+}
+
 module.exports = {
-    somar
+    somar, procurarUsuario
 }
--- tests/programTest.js
+++ tests/programTest.js
@@ -8,4 +8,10 @@ describe('user list ', ()=> {
     it('deve somar dois numeros diferentes e retornar 30', () => {
         myProgram.somar(20,10).should.be.equal(30)
     })
+    it('Passar um nome e deve retornar: Usuário não encontrado', () => {
+        myProgram.procurarUsuario('Leonardo Soares').should.be.equal('Usuário não encontrado')
+    })
+    it('Retorna mensagem de que o nome é obrigatorio', () => {
+        myProgram.procurarUsuario('').should.be.equal('Campo nome esta vazio')
+    })
 })
